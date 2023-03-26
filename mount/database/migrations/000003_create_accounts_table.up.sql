8e7c64a6d3a5bfb32abf748e8c248b89d05fc2b2
cmyui <cmyuiosu@gmail.com>
2023-03-25 23:14:03 -0400

    add accounts table sql

---
+++ mount/database/migrations/000003_create_accounts_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE accounts (
+    id SERIAL PRIMARY KEY,
+    phone_number TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    status TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+CREATE INDEX accounts_phone_number ON accounts (phone_number);
+CREATE INDEX accounts_status ON accounts (status);
