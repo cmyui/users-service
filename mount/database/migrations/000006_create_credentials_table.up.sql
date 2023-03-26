825edf7076bb5e6366d7f03c19177101d9ab31ef
cmyui <cmyuiosu@gmail.com>
2023-03-26 08:52:11 -0400

    make credentials identifier unique

--- mount/database/migrations/000006_create_credentials_table.up.sql
+++ mount/database/migrations/000006_create_credentials_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE credentials (
     rec_id SERIAL PRIMARY KEY,
     credentials_id UUID NOT NULL UNIQUE,
     account_id UUID NOT NULL,
-    identifier TEXT NOT NULL,
+    identifier TEXT NOT NULL UNIQUE,
     secret TEXT NOT NULL,
     status TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
