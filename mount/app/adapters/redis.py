cd866e63f302b6d4adc7cdcf7ec0ead16a5b0a14
cmyui <cmyuiosu@gmail.com>
2023-03-26 07:51:10 -0400

    remove redis dsn generation code

--- mount/app/adapters/redis.py
+++ mount/app/adapters/redis.py
@@ -1,10 +1,1 @@
-def get_dsn(
-    scheme: str,
-    host: str,
-    port: int,
-    database: int,
-) -> str:
-    return f"{scheme}://{host}:{port}/{database}"
-
-
 # TODO: write a class to divide read & write calls between primary & replicas
